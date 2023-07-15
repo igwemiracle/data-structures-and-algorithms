990f615c2112e4d35d2960adf5546756c25b337d
igwemiracle <igwemiracle35@gmail.com>
2023-07-15 23:27:25 +0100

    data structures and algorithm implementations

--- hash.py
+++ hash.py
@@ -97,8 +97,25 @@ def Union(List1, List2):
         ans.InsertNode(key)
     return ans.head
     
+def printPairs(arr, arr_size, sum):
+ 
+    # Create an empty hash map
+    # using an hashmap allows us to store the indices
+    hashmap = {}
+ 
+    for i in range(0, arr_size):
+        diff = sum-arr[i]
+        if (diff in hashmap):
+            print(f"Yes! there is a pair ({diff},{arr[i]})")
+            return
+        hashmap[arr[i]] = i
+    print("No")
+ 
+ 
 
-
+A = [1, 4, 45, 6, 10, 8]
+n = 16
+printPairs(A, len(A), n)
 
 ll1 = LinkedList()
 ll1.InsertNode(1)
