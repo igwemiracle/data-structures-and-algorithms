1e7a238bf1c90f4c26b0f06b4f9add432032609f
igwemiracle <igwemiracle35@gmail.com>
2023-07-17 17:45:05 +0100

    data structures and algorithm implementations

--- rough_wrk.py
+++ rough_wrk.py
@@ -195,28 +195,11 @@ class Conversion:
 		for ch in self.output:
 			print(ch, end=" ")
 
-def PrefixToInfix(expression):
-    #stack container
-    stack = []
-    #Reading the expression from right-to-left
-    exp = expression[::-1]
-    for char in exp:
-        #Check for operand
-        if char.isalnum():
-            stack += char
-        #Check for operator
-        elif char=="(" or char==")" or char=="+" or char=="-" or char=="*" or char=="/":
-            operand1 = stack.pop()
-            operand2 = stack.pop()
-            str = '(' + operand1 + char + operand2 + ')'
-            stack.append(str)
-    return stack.pop()
-                   
 
 
-#❌ Ques = input("Enter prefix: ")
-# answer = PrefixToInfix(Ques)
-# print("Infix value is: ", answer)
+
+
+
         
 
 # ❌# Driver code
