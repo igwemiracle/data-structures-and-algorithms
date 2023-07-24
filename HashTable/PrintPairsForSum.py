f323e298dd6d23dc515d262296578f5b5dfc0c11
igwemiracle <igwemiracle35@gmail.com>
2023-07-24 20:34:45 +0100

    data structures and algorithm implementations

--- HashTable/PrintPairsForSum.py
+++ HashTable/PrintPairsForSum.py
@@ -1,9 +1,9 @@
 def printPairs(arr, arr_size, sum):
- 
+
     # Create an empty hash map
     # using an hashmap allows us to store the indices
     hashmap = {}
- 
+
     for i in range(0, arr_size):
         diff = sum-arr[i]
         if (diff in hashmap):
@@ -11,8 +11,7 @@ def printPairs(arr, arr_size, sum):
             return
         hashmap[arr[i]] = i
     print("No")
- 
- 
+
 
 A = [1, 4, 45, 6, 10, 8]
 n = 16
