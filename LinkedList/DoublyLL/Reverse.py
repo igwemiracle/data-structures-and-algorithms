393ff11bd26575c2893176e5cf15dc562deb5478
igwemiracle <igwemiracle35@gmail.com>
2023-07-24 20:55:05 +0100

    data structures and algorithm implementations

--- LinkedList/DoublyLL/Reverse.py
+++ LinkedList/DoublyLL/Reverse.py
@@ -1,43 +1,44 @@
 class Node:
-	def __init__(self, data):
-		self.data = data
-		self.next = None
-		self.prev = None
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+        self.prev = None
+
 
 class DoublyLinkedList:
-	def __init__(self):
-		self.head = None
+    def __init__(self):
+        self.head = None
 
-	def append(self, data):
-		if self.head is None:
-			new_node = Node(data)  
-			self.head = new_node  
-		else:                     
-			new_node = Node(data) 
-			cur = self.head        
-			while cur.next:        
-				cur = cur.next     
-			cur.next = new_node    
-			new_node.prev = cur    
+    def append(self, data):
+        if self.head is None:
+            new_node = Node(data)
+            self.head = new_node
+        else:
+            new_node = Node(data)
+            cur = self.head
+            while cur.next:
+                cur = cur.next
+            cur.next = new_node
+            new_node.prev = cur
 
-	def print_list(self):
-		cur = self.head
-		while cur:
-			print(cur.data, end=" ")
-			cur = cur.next
+    def print_list(self):
+        cur = self.head
+        while cur:
+            print(cur.data, end=" ")
+            cur = cur.next
+
+    def reverse(self):
+        tmp = None
+        cur = self.head
+        while cur:
+            tmp = cur.prev
+            cur.prev = cur.next
+            cur.next = tmp
+            cur = cur.prev
+        if tmp:
+            self.head = tmp.prev
 
 
-	def reverse(self):
-		tmp = None
-		cur = self.head
-		while cur:
-			tmp = cur.prev
-			cur.prev = cur.next
-			cur.next = tmp
-			cur = cur.prev
-		if tmp:
-			self.head = tmp.prev
-			
 dllist = DoublyLinkedList()
 dllist.append(1)
 dllist.append(2)
