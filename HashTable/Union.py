d2b939b4a4131d7fa42a67fc349f346f1e3a63aa
igwemiracle <igwemiracle35@gmail.com>
2023-07-24 20:40:34 +0100

    data structures and algorithm implementations

--- HashTable/Union.py
+++ HashTable/Union.py
@@ -1,14 +1,15 @@
 class Node:
-    def __init__(self,data):
+    def __init__(self, data):
         self.data = data
         self.next = None
+
 
 class LinkedList:
     def __init__(self):
         self.head = None
         self.tail = None
 
-    def InsertNode(self,new_data):
+    def InsertNode(self, new_data):
         if self.head is None:
             self.head = Node(new_data)
             self.tail = self.head
@@ -21,6 +22,8 @@ class LinkedList:
 # For Union: Traverse both the lists, store the elements in Hash-map and update the respective count.
 # AUXILIARY SPACE--> O(m+n). Use of Hashmap data structure for storing values.
 # Return the head of new list containing the Union of 2 linkedList
+
+
 def Union(List1, List2):
     HashMap = {}
     while (List1 != None):
@@ -28,18 +31,19 @@ def Union(List1, List2):
         if (data not in HashMap.keys()):
             HashMap[data] = 1
         List1 = List1.next
-    
+
     while (List2 != None):
         data = List2.data
         if (data not in HashMap.keys()):
             HashMap[data] = 1
         List2 = List2.next
-    #making a new list
+    # making a new list
     ans = LinkedList()
-    #traverse HashMap
-    for key,value in HashMap.items():
+    # traverse HashMap
+    for key, value in HashMap.items():
         ans.InsertNode(key)
     return ans.head
+
 
 def printList(head):
     while head:
@@ -47,23 +51,25 @@ def printList(head):
         head = head.next
     print()
 
+
 ll1 = LinkedList()
 ll1.InsertNode(1)
 ll1.InsertNode(2)
 ll1.InsertNode(3)
 ll1.InsertNode(4)
 ll1.InsertNode(5)
-  
+
 ll2 = LinkedList()
 ll2.InsertNode(1)
 ll2.InsertNode(3)
 ll2.InsertNode(5)
 ll2.InsertNode(2)
- 
+
 print("First list is ")
 printList(ll1.head)
-
-print("Second list is ")
+print("")
+print("Second list is")
 printList(ll2.head)
+print("")
 print("Union list is ")
 printList(Union(ll1.head, ll2.head))
